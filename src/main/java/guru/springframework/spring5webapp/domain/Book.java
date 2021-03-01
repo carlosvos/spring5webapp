1ff9ba6b367f235ea8c278a78abb2c0a2e31fd9e
carloso <carlos.oliveira@me.com.br>
2021-03-01 11:19:30 -0300

    github credential test

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -12,6 +12,7 @@ public class Book {
 
     private String title;
     private String isbn;
+    private String teste;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn("book_id"),
